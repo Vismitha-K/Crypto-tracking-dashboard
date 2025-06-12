6d9ce82f64b40a75380b87d243e1c26918f72314
Vismitha-K <vismithashetty24@gmail.com>
2025-06-12 19:19:39 +0530

    Delete src directory

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cryptoReducer from "./cryptoSlice";
-
-export const store = configureStore({
-    reducer: {
-        crypto: cryptoReducer,
-    },
-});
